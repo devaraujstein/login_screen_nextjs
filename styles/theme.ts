e145c3091000ca9d7b6767a1485f7746252c63fe
devaraujstein <andreearaujo98@gmail.com>
2020-08-20 14:39:59 -0300

    Chakra and Emotion configured

---
+++ styles/theme.ts
@@ -0,0 +1,8 @@
+import { theme, DefaultTheme } from 'chakra-ui/core';
+import { theme, DefaultTheme } from '@chakra-ui/core';
+
+const customTheme: DefaultTheme = {
+    ...theme
+}
+
+export default customTheme;
